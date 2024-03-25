c0d5cfb2b932a9aeb11272894097606185d49f29
roloxalien <164809034+roloxalien@users.noreply.github.com>
2024-03-25 13:36:07 -0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# homework :)
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/roloxalien/pen/QWPgyxE](https://codepen.io/roloxalien/pen/QWPgyxE).
+
---
+++ script.js
@@ -0,0 +1,29 @@
+var cash = 0
+var price1 = 30
+var size = 3
+
+
+
+
+
+//for (var i = 0; i < Infinity; i++) {
+ 
+ 
+  
+  
+//
+
+function myFunction() {
+  
+  cash = cash + 1
+  
+  document.getElementById("score").innerHTML = "$"+cash;
+  
+  
+  
+ 
+document.getElementById("score").style.fontSize = size +'rem';
+  
+}
+
+//window.onload = var1;
